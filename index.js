4153cc4c822a78e6b1b4ca1af719fa5f912e322a
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2022-10-16 13:03:19 +0300

    Photo update

--- index.js
+++ index.js
@@ -3,16 +3,3 @@
 /* CSS animation classes settings |
  Configuración de las clases para las animaciones */
 const allCards = document.querySelectorAll('.container-member');
-
-
-
-
-/* Footer link settings |
- Configuración del enlace del Footer */
-const footerLink = document.querySelector('#footer a'),
-      profiles = ['https://github.com/javiervaleriano',
-      'https://codepen.io/javiervaleriano',
-      'https://www.linkedin.com/in/javier-valeriano1508/',
-      'https://devchallenges.io/portfolio/javiervaleriano'];
-
-footerLink.href = profiles[Math.floor(Math.random() * profiles.length)];
